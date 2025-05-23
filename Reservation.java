4f50de984e44c48d9eb9f9bac3ef807ddb90abfd
Fahmida-hash <2023-3-60-048@std.ewubd.edu>
2025-05-23 20:52:17 +0600

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hotelmanagementsystem;
 import java.util.*;
-/**
- *
- * @author Digital Outlet
- */
+
 public class Reservation {
     private Customer customer;
     private Room room;
